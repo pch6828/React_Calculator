1adfd97d16ebae5dd255ec25bf898f1ee42c3e3d
pch6828 <pch6828@hanyang.ac.kr>
2021-06-30 16:19:23 +0900

    6:30 : minor css change

--- src/Screen.js
+++ src/Screen.js
@@ -4,14 +4,13 @@ import "./Screen.css";
 
 function Screen({expression, show_prev_expression}){
     const history_size = Number(window.sessionStorage.history_size);
-    const prev_expression = (history_size===0||isNaN(history_size))?null:(show_prev_expression?window.sessionStorage["expression_"+history_size]:"Ans");
-    const prev_answer = history_size===0?null:window.sessionStorage["answer_"+history_size];
-
-    const total_prev = prev_expression?(prev_expression+" = "+prev_answer):null;
-    const now_expression = expression===undefined?"0":expression;
+    const latest_answer = (history_size===0||isNaN(history_size))?null:window.sessionStorage["answer_"+history_size];
+    const prev_expression = (history_size===0||isNaN(history_size))?null:(show_prev_expression?(window.sessionStorage["expression_"+history_size] + " = "):("Ans = "+latest_answer));
+    
+    const now_expression =show_prev_expression?latest_answer:(expression===undefined?"0":expression);
     return (
         <div className="screen">
-            <span className="prev_expression">{total_prev}</span>
+            <span className="prev_expression">{prev_expression}</span>
             <span className="now_expression">{now_expression}</span>
         </div>
     );
